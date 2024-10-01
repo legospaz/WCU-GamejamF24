d3ca245ba4c2d99f9852be27fe09d598536843cd
BlitzLego <alec.creely@gmail.com>
2024-10-01 16:25:10 -0400

    added player script

---
+++ Game Jam/Assets/Player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	move_and_slide()
