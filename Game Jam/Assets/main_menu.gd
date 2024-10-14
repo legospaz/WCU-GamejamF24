6b5ecf1720b4679bc1b0f33e0601de30b0d4af7c
BlitzLego <alec.creely@gmail.com>
2024-10-14 12:10:06 -0400

    Created Main Menu

---
+++ Game Jam/Assets/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game Jam/Assets/Level.tscn")
+
+
+func _on_quit_pressed() -> void:
+	pass
